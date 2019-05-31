a2313931b3f2c8c92130d1b14c6fb3ec6456b549
ken1286 <kindle1286@gmail.com>
2019-05-31 08:28:49 -0700

    project router complete

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -5,7 +5,81 @@ const actionDb = require('../data/helpers/actionModel.js');
 
 // CRUD
 
+router.get('/', (req, res) => {
 
+  projectDb.get()
+    .then(projects => {
+      res.status(200).json({ projects });
+    })
+    .catch( err => {
+      res.status(500).json({ error: "Project information could not be retrieved."})
+    })
+});
+
+router.get('/:id', validateProjectId, (req, res) => {
+  res.status(200).json(req.project);
+});
+
+router.post('/', validateProject, (req, res) => {
+  const newProject = req.body;
+
+  projectDb.insert(newProject)
+    .then( project => {
+      res.status(200).json({ project });
+    })
+    .catch( err => {
+      res.status(500).json({ error: "There was an error while saving the project to the database" })
+    })
+});
+
+router.put('/:id', validateProjectId, validateProject, (req, res) => {
+  const updatedProject = req.body;
+  const { id } = req.params;
+
+  projectDb.update(id, updatedProject)
+    .then( project => {
+      res.status(204).json({project});
+    })
+    .catch( err => {
+      res.status(500).json({ message: 'Error updating project.' });
+    })
+});
+
+router.get('/:id/actions', validateProjectId, (req, res) => {
+  const { id } = req.params;
+
+  projectDb.getProjectActions(id)
+    .then( actions => {
+      res.status(200).json({actions});
+    })
+    .catch( err => {
+      res.status(500).json({ error: 'Could not access project actions.'});
+    })
+});
+
+router.post('/:id/actions', validateProjectId, validateAction, (req, res) => {
+  const newAction = {...req.body, project_id: req.params.id};
+
+  actionDb.insert(newAction)
+    .then( action => {
+      res.status(200).json({ action });
+    })
+    .catch( err => {
+      res.status(500).json({ error: "There was an error while saving the action to the database" })
+    })
+});
+
+router.delete('/:id', validateProjectId, (req, res) => {
+  const { id } = req.params;
+
+  projectDb.remove(id)
+    .then( project => {
+      res.status(204).json({ project })
+    })
+    .catch( err => {
+       res.status(500).json({ message: 'error' });
+    })
+});
 
 // middleware
 
